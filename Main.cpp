f51e966bbc20d2ed75cfe6b8ab49da30d216c850
Zenn <149383000+Cenx-bash@users.noreply.github.com>
2025-08-08 14:02:50 +0800

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+
+using namespace std; 
+ int main() {
+     
+  int number;
+   
+  cout << " Enter Number: "; // << endl; or \n 
+   
+  cin  >> number;
+   
+ cout << " That number that you entered is " << number;
+
+   int num1, num2, product;
+    
+   cout << "Enter an Number: ";
+   cin >> num1;
+    cout << "Enter Other Numbers: ";
+    cin >> num2;
+    
+    product = num1 + num2;
+    cout << "the sum of the numbers is : " << product;
+  
+    return 0;
+ }
+ 
